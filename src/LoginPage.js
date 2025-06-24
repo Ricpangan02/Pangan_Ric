a8a0ef3d04f4dad6f3ff581d6fc4c14666461a96
OgDoug <ricpangan02@gmail.com>
2025-06-24 15:24:55 +0800

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,21 +1,37 @@
 import React, { useState } from 'react';
+import './LoginPage.css';
 
-function LoginPage() {
- 
+function LoginPage({ onBackClick, onLoginSuccess }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (username === 'ric' && password === '1234') {
+      onLoginSuccess(); // go to dashboard
+    } else {
+      alert('Invalid credentials!');
+    }
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-       
-      /><br />
-      <input
-        type="password"
-        placeholder="Password"
-     
-      /><br />
-      <button >Login</button>
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+        <button className="back-button" onClick={onBackClick}>Back</button>
+      </div>
     </div>
   );
 }
